183ab84fa2fdea77b8e7efbb6978e6ecbb057adb
miya789 <miya.nori42395@gmail.com>
2020-04-10 18:22:23 +0900

    Modify name of TO, BCC

--- Executive/send_mail_to_executive.sh
+++ Executive/send_mail_to_executive.sh
@@ -204,9 +204,9 @@ touch ${TMP}
 # 4.4 文面ファイル(tmp.txt)の執筆
 (
   echo "From: ${FROM}"
-  echo "To: ${TO}"
-  if [ "$BCC" != "" ]; then
-    echo "Bcc: ${BCC}"
+  echo "To: ${EXECUTIVE}"
+  if [ "$EXECUTIVE_BCC" != "" ]; then
+    echo "Bcc: ${EXECUTIVE_BCC}"
   fi
   echo "Subject: ${SUBJECT_ENC}"
   echo "Content-Type: text/plain; charset=UTF-8"
@@ -239,7 +239,7 @@ touch ${TMP}
 ) >> ${TMP}
 
 # 4.5 メールの送信
-# cat ${TMP} | $SENDMAIL_PATH -i -f ${FROM} ${TO} # BCC使わなければこっちが安全
+# cat ${TMP} | $SENDMAIL_PATH -i -f ${FROM} ${EXECUTIVE} # Bcc使わなければこっちが安全
 cat ${TMP} | $SENDMAIL_PATH -i -t
 
 # 4.6 文面ファイル(tmp.txt)をログへ吐き出し
