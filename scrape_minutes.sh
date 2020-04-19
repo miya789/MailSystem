e32ea98fa7c163e54065b6cb9a23755a7fa81d00
miya789 <miya.nori42395@gmail.com>
2020-04-20 00:18:17 +0900

    [WIP]Add the protoype scraping codes. It works probably in terms of preparing params, but doesn't get valid update urls and prepare generating msg to be posted yet.

--- scrape_minutes.sh
+++ scrape_minutes.sh
@@ -41,57 +41,130 @@ if [ $should_generate_minute -eq 0 ]; then
   exit 0
 fi
 
-curl --socks5 $PROXY $MINUTES_TOP_URL --digest -u $USER:$PASSWORD > top_page.html
-sleep 1
-TeamMEMS=`grep 'meeting 議事録</a>' top_page.html | sed -n 's/^.*href="\([^"]*\)".*$/\1/p' | head -1 | tail -1`
-Executive=`grep 'meeting 議事録</a>' top_page.html | sed -n 's/^.*href="\([^"]*\)".*$/\1/p' | head -2 | tail -1`
+# Preparation tmp html files
+TOP_HTML="${TMP_DIR}/top.html"
+INDEX_HTML="${TMP_DIR}/index.html"
+INDEX_EDIT_HTML="${TMP_DIR}/index_edit.html"
+TARGET_HTML="${TMP_DIR}/target.html"
+TARGET_EDIT_HTML="${TMP_DIR}/target_edit.html"
+INDEX_ORIGINAL_TXT="${TMP_DIR}/index_original.txt"
+index_msg_txt="${TMP_DIR}/index_msg.txt"
+TARGET_ORIGINAL_TXT="${TMP_DIR}/target_original.txt"
+target_msg_txt="${TMP_DIR}/target_msg.txt"
 
-echo "TeamMEMS: ${TeamMEMS}"
-echo "Executive: ${Executive}"
+TODAY_FOR_MINUTES=`date +%Y%m%d`
 
-curl --socks5 $PROXY $TeamMEMS --digest -u $USER:$PASSWORD > teamMEMS.html
+CURL_OPTIONS="--socks5 ${PROXY} --digest -u ${USER}:${PASSWORD}"
 
-date=`date +%Y/%m/%d` # 危険なので暫定的に
+# Scraping
+echo "  Getting top_page.html..."
+curl ${MINUTES_TOP_URL} ${CURL_OPTIONS} > ${TOP_HTML}
+sleep 5
+if [ "${MEETING_SUBJECT}" = "TeamMEMS meeting" ]; then
+  INDEX_URL=`grep 'meeting 議事録</a>' ${TOP_HTML} | sed -n 's/^.*href="\([^"]*\)".*$/\1/p' | head -1 | tail -1`
+  echo ${INDEX_URL}
+elif [ "${MEETING_SUBJECT}" = "Executive meeting" ]; then
+  INDEX_URL=`grep 'meeting 議事録</a>' ${TOP_HTML} | sed -n 's/^.*href="\([^"]*\)".*$/\1/p' | head -2 | tail -1`
+fi
+echo "    INDEX_URL: ${INDEX_URL}"
+echo "  Finished!"
+echo
 
-next_url=`grep "${date}" teamMEMS.html | sed -n 's/^.*href=\"\([^"]*\)".*$/\1/p' | tail -1`
-sleep 1
+echo "  Getting index.html..."
+curl ${INDEX_URL} ${CURL_OPTIONS} > ${INDEX_HTML}
+sleep 5
+TARGET_URL=`grep "${TODAY_FOR_MINUTES}" ${INDEX_HTML} | sed -n 's/^.*href=\"\([^"]*\)".*$/\1/p' | tail -1`
+echo "    TARGET_URL:${TARGET_URL}"
+echo "  Finished!"
+echo
 
-curl --socks5 $PROXY $next_url --digest -u $USER:$PASSWORD > teamMEMS_contents.html
-edit_url=`grep "Edit" teamMEMS_contents.html | sed -n 's/^.*href=\"\([^"]*\)".*$/\1/p' | sed -e 's/\&amp\;/\&/g' | head -1 | tail -1`
-sleep 1
+# while [ "${TARGET_URL}" = "" ]; do
+#   echo "  Cannot find today minutes, so generating page..."
+#   INDEX_EDIT_URL=`grep "Edit" ${INDEX_HTML} | sed -n 's/^.*href=\"\([^"]*\)".*$/\1/p' | sed -e 's/\&amp\;/\&/g' | head -1 | tail -1`
+#   echo "    INDEX_EDIT_URL:${INDEX_EDIT_URL}"
+#   echo
+#   echo "  Getting index_edit.html..."
+#   curl ${INDEX_EDIT_URL} ${CURL_OPTIONS} > ${INDEX_EDIT_HTML}
+#   echo
+#   sleep 5
 
-echo $test_edit
-curl --socks5 $PROXY $edit_url --digest -u $USER:$PASSWORD > teamMEMS_edit.html
 
-digest=`cat teamMEMS_edit.html | grep digest | sed -n 's/^.* value=\"\([^"]*\).*/\1/p'`
-cat teamMEMS_edit.html | sed -ne '/<textarea name=\"original/,/<\/textarea>/p' | sed 's/  \(<[^>]*\)/\1/g' | sed -e 's/<[^>]*>//g' | sed '1,2d' > original.txt # 初めの二行はテンプレート
 
-echo digest
-echo $digest
-echo original
-cat original.txt
+#   # params作成現場
+#   INDEX_DIGEST=`cat "${INDEX_EDIT_HTML}" | grep digest | sed -n 's/^.* value=\"\([^"]*\).*/\1/p'`
+#   cat ${INDEX_EDIT_HTML} | sed -ne '/<textarea name=\"original/,/<\/textarea>/p' | sed 's/  \(<[^>]*\)/\1/g' | sed -e 's/<[^>]*>//g' | sed '1,2d' > ${INDEX_ORIGINAL_TXT}
 
-####################################################################################################################
-#                                       ここで，tmp.txtを元に，msg.txtを作成                                         #
-####################################################################################################################
+#   cp ${INDEX_ORIGINAL_TXT} ${index_msg_txt}
+#   INSERTING_TXT="" # 新しい議事録のURLなどを挿入する行
+#   sed -i "1s/^/${INSERTING_TXT}\n/" ${index_msg_txt}
 
-msg_enc=`cat msg.txt | nkf -WwMQ | sed -e ':loop; N; $!b loop; s/=\n//g' | sed -z 's/\n/%0D%0A/g' | tr = % | tr -d '\n'`
-original_enc=`cat original.txt | nkf -WwMQ | sed -e ':loop; N; $!b loop; s/=\n//g' | sed -z 's/\n/%0D%0A/g' | tr = % | tr -d '\n'`
+#   INDEX_MSG_ENC=`cat ${index_msg_txt} | nkf -WwMQ | sed -e ':loop; N; $!b loop; s/=\n//g' | sed -z 's/\n/%0D%0A/g' | tr = % | tr -d '\n'`
+#   INDEX_ORIGINAL_ENC=`cat ${INDEX_ORIGINAL_TXT} | nkf -WwMQ | sed -e ':loop; N; $!b loop; s/=\n//g' | sed -z 's/\n/%0D%0A/g' | tr = % | tr -d '\n'`
 
-# echo "${original_enc}" | nkf -W --url-input | sed -z 's/%0A/\n/g' > tmp2.txt
-# echo $original_enc
-# cat tmp2.txt
+#   # 各パラメータの値の確認が必要
+#   ## encode_hit="ぷ"
+#   ## cmd="edit"
+#   ## digest="<毎回異なるハッシュ値らしきもの>"
+#   ## msg="<投稿内容>"
+#   ## original="<元の内容>"
+#   ## write="Update"
+#   INDEX_PARAMS="encode_hint=%E3%81%B7&cmd=edit&digest=${INDEX_DIGEST}&msg=${INDEX_MSG_ENC}&original=${INDEX_ORIGINAL_ENC}&write=Update"
+#   echo "    INDEX_PARAMS:${INDEX_PARAMS}"
+#   echo
+
+
+
+#   INDEX_UPDATE_URL=`grep "Edit" ${INDEX_EDIT_HTML} | sed -n 's/^.*href=\"\([^"]*\)".*$/\1/p' | sed -e 's/\&amp\;/\&/g' | head -1 | tail -1`
+#   echo "    INDEX_UPDATE_URL:${INDEX_UPDATE_URL}"
+#   # 怖いので，後日似た場所を用意し，個別でエスケープすべき文字があるか確認テスト
+#   # curl ${INDEX_UPDATE_URL} ${CURL_OPTIONS} -XPOST -d "${INDEX_PARAMS}"
+#   sleep 5
+
+
+
+#   TARGET_URL=`grep "${TODAY_FOR_MINUTES}" ${INDEX_HTML} | sed -n 's/^.*href=\"\([^"]*\)".*$/\1/p' | tail -1`
+# done
+# echo "Preparaing page finished!"
+# echo
+
+curl ${TARGET_URL} ${CURL_OPTIONS} > ${TARGET_HTML}
+sleep 5
+
+echo "  Searching url for updating target page..."
+TARGET_EDIT_URL=`grep "Edit" ${TARGET_HTML} | sed -n 's/^.*href=\"\([^"]*\)".*$/\1/p' | sed -e 's/\&amp\;/\&/g' | head -1 | tail -1`
+# For test>>>
+TARGET_EDIT_URL="http://mozart.if.t.u-tokyo.ac.jp/memswiki/index.php?cmd=edit&page=%E3%83%9F%E3%83%BC%E3%83%86%E3%82%A3%E3%83%B3%E3%82%B0%E8%AD%B0%E4%BA%8B%E9%8C%B2/20200423&refer=%E3%83%9F%E3%83%BC%E3%83%86%E3%82%A3%E3%83%B3%E3%82%B0%E8%AD%B0%E4%BA%8B%E9%8C%B2"
+# <<<For test
+echo "    TARGET_EDIT_URL:${TARGET_EDIT_URL}"
+curl ${TARGET_EDIT_URL} ${CURL_OPTIONS} > ${TARGET_EDIT_HTML}
+sleep 5
+
+
+# params作成現場
+TARGET_DIGEST=`cat "${TARGET_EDIT_HTML}" | grep digest | sed -n 's/^.* value=\"\([^"]*\).*/\1/p'`
+cat ${TARGET_EDIT_HTML} | sed -ne '/<textarea name=\"original/,/<\/textarea>/p' | sed 's/  \(<[^>]*\)/\1/g' | sed -e 's/<[^>]*>//g' | sed '1,2d' > ${TARGET_ORIGINAL_TXT} # 初めの二行はテンプレート
+
+cp ${TARGET_ORIGINAL_TXT} ${target_msg_txt}
+INSERTING_TXT="" # 書く内容を用意
+sed -i "1s/^/${INSERTING_TXT}\n/" ${target_msg_txt}
+
+TARGET_MSG_ENC=`cat ${target_msg_txt} | nkf -WwMQ | sed -e ':loop; N; $!b loop; s/=\n//g' | sed -z 's/\n/%0D%0A/g' | tr = % | tr -d '\n'`
+TARGET_ORIGINAL_ENC=`cat ${TARGET_ORIGINAL_TXT} | nkf -WwMQ | sed -e ':loop; N; $!b loop; s/=\n//g' | sed -z 's/\n/%0D%0A/g' | tr = % | tr -d '\n'`
 
 # 各パラメータの値の確認が必要
-params="encode_hint=%E3%81%B7"\
-"&cmd=edit"\
-"&digest=${digest}"\
-"&msg=${msg_enc}"\
-"&original=${original_enc}"\
-"&write=Update"
-echo params
-# "&template_page="\
+## encode_hit="ぷ"
+## cmd="edit"
+## digest="<毎回異なるハッシュ値らしきもの>"
+## msg="<投稿内容>"
+## original="<元の内容>"
+## write="Update"
+TARGET_PARAMS="encode_hint=%E3%81%B7&cmd=edit&digest=${TARGET_DIGEST}&msg=${TARGET_MSG_ENC}&original=${TARGET_ORIGINAL_ENC}&write=Update"
+echo "    TARGET_PARAMS:${TARGET_PARAMS}"
 
-echo ${params} > params.txt
 
-curl --socks5 $PROXY $edit_url --digest -u $USER:$PASSWORD -XPOST -d "$params" > teamMEMS_test_edit.html
+
+TARGET_UPDATE_URL=`grep "Edit" ${TARGET_EDIT_HTML} | sed -n 's/^.*href=\"\([^"]*\)".*$/\1/p' | sed -e 's/\&amp\;/\&/g' | head -1 | tail -1` # ここのエスケープ個別変換は何!?!?!?!?!?!??!?!?!?!?!?!??!?!?!?! アドレスが違って，"Using both cmd= and plugin= is not allowed"とか言われるんやが，，，
+echo "    TARGET_UPDATE_URL:${TARGET_UPDATE_URL}"
+# 怖いので，後日似た場所を用意し，個別でエスケープすべき文字があるか確認テスト
+curl ${TARGET_UPDATE_URL} ${CURL_OPTIONS} -XPOST -d "${TARGET_PARAMS}" >> result.txt
+sleep 5
