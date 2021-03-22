1d5563d96ead7cc90982397eddabfe64886adba5
miya789 <miya.nori42395@gmail.com>
2021-03-22 16:55:13 +0900

    Move getter flag into pkg

--- cmd/mail_reminder.go
+++ cmd/mail_reminder.go
@@ -1,34 +1,16 @@
 package main
 
 import (
+	"LabMeeting/pkg/lab_flag"
 	"LabMeeting/pkg/lab_mail"
-	"LabMeeting/pkg/meeting_type"
 	"LabMeeting/pkg/schedule"
-	"flag"
-	"fmt"
 	"log"
-	"os"
-	"strconv"
 	"time"
 )
 
-func getMeetingType() (meeting_type.MeetingType, error) {
-	var mtg int
-	flag.IntVar(&mtg, "mtg", 0, "0: others \t(default, but do not use)\n1: TeamMEMS\n2: Executive")
-	flag.Parse()
-	if mtg <= int(meeting_type.Unknown) || mtg > int(meeting_type.Executive) {
-		err := fmt.Errorf("invalid value %q for flag -%s: %v", strconv.Itoa(mtg), "mtg", "parse error")
-		fmt.Fprintln(flag.CommandLine.Output(), err)
-		fmt.Fprintf(flag.NewFlagSet(os.Args[0], flag.ExitOnError).Output(), "Usage of %s:\n", os.Args[0])
-		flag.PrintDefaults()
-		return 0, err
-	}
-	return meeting_type.MeetingType(mtg), nil
-}
-
 func main() {
 	// Determine meeting type
-	mtg, err := getMeetingType()
+	mtg, err := lab_flag.GetMeetingType()
 	if err != nil {
 		log.Println(err)
 		return
