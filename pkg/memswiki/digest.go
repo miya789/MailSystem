34f1a9449b99d8e004ee845ad1be2229099ec9d5
miya789 <miya.nori42395@gmail.com>
2021-03-23 17:56:42 +0900

    Add codes to send minutes into memswiki pkg

---
+++ pkg/memswiki/digest.go
@@ -0,0 +1,161 @@
+package memswiki
+
+import (
+	"bytes"
+	"crypto/md5"
+	"crypto/rand"
+	"encoding/hex"
+	"fmt"
+	"io"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"strings"
+
+	"github.com/PuerkitoBio/goquery"
+)
+
+func digestPost(method, host, uri string, additionalHeaders map[string]string, payload io.Reader) ([]byte, error) {
+	fmt.Println()
+	req, err := http.NewRequest(method, host+uri, nil)
+	req.Header.Set("Content-Type", "application/json")
+	client := &http.Client{
+		Transport: &http.Transport{Proxy: http.ProxyURL(proxyURL)},
+	}
+	resp, err := client.Do(req)
+	if err != nil {
+		return nil, err
+	}
+	defer resp.Body.Close()
+	log.Printf("getting digest key...\n\x1b[34m%s\x1b[0m\n", resp)
+	if resp.StatusCode != http.StatusUnauthorized {
+		return nil, fmt.Errorf("Recieved status code '%v' auth skipped", resp.StatusCode)
+	}
+
+	digestParts := getDigestParts(resp)
+	digestParts["uri"] = uri
+	digestParts["method"] = method
+	digestParts["username"] = username
+	digestParts["password"] = password
+	req, err = http.NewRequest(method, host+uri, payload)
+	req.Header.Set("Authorization", getDigestAuthrization(digestParts))
+	req.Header.Set("Content-Type", "application/json")
+	// log.Printf("digestParts: \n\x1b[33m%s\x1b[0m\n", digestParts)
+	// log.Printf("getDigestAuthrization(digestParts): \n\x1b[33m%s\x1b[0m\n", getDigestAuthrization(digestParts))
+	for k, v := range additionalHeaders {
+		req.Header.Set(k, v)
+	}
+	resp, err = client.Transport.RoundTrip(req)
+	// resp, err = client.Do(req)
+	if err != nil {
+		return nil, err
+	}
+	defer resp.Body.Close()
+	body, err := ioutil.ReadAll(resp.Body)
+	// log.Printf("req.Header:\n\x1b[33m%s\x1b[0m\n", req.Header)
+	// log.Printf("resp.Request.Header:\n\x1b[33m%s\x1b[0m\n", resp.Request.Header)
+	if resp.StatusCode != http.StatusOK {
+		if err != nil {
+			return nil, err
+		}
+		if resp.StatusCode >= 300 && resp.StatusCode < 400 {
+			next, err := resp.Location()
+			if err != nil {
+				return nil, fmt.Errorf("ErrStatusContradiction")
+			}
+			log.Printf("Redirect:\n\x1b[33m%s\x1b[0m\n", resp)
+
+			host := next.Scheme + "://" + next.Host
+			uri := next.Path + "?" + next.RawQuery
+			log.Printf("next location:\nhost:\n\x1b[33m%s\x1b[0m\nuri:\n\x1b[33m%s\x1b[0m\nurl:\n\x1b[33m%s\x1b[0m\n", host, uri, host+uri)
+			redirectRes, err := digestPost(http.MethodGet, host, uri, additionalHeaders, nil)
+			if err != nil {
+				return redirectRes, fmt.Errorf("failed to request: %w", err)
+			}
+			return redirectRes, nil
+		}
+		log.Printf("not OK:\n\x1b[31m%s\x1b[0m\n", resp)
+		log.Printf("not OK string(body):\n\x1b[31m%s\x1b[0m\n", string(body))
+		return nil, fmt.Errorf("401 desu")
+	}
+	log.Printf("Success:\n\x1b[32m%s\x1b[0m\n", resp)
+
+	return body, nil
+}
+
+func getDigestParts(resp *http.Response) map[string]string {
+	result := map[string]string{}
+	if len(resp.Header["Www-Authenticate"]) > 0 {
+		wantedHeaders := []string{"nonce", "realm", "qop"}
+		responseHeaders := strings.Split(resp.Header["Www-Authenticate"][0], ",")
+		for _, r := range responseHeaders {
+			for _, w := range wantedHeaders {
+				if strings.Contains(r, w) {
+					result[w] = strings.Split(r, `"`)[1]
+				}
+			}
+		}
+	}
+	return result
+}
+
+func getMD5(text string) string {
+	hasher := md5.New()
+	hasher.Write([]byte(text))
+	return hex.EncodeToString(hasher.Sum(nil))
+}
+
+func getCnonce() string {
+	b := make([]byte, 8)
+	io.ReadFull(rand.Reader, b)
+	return fmt.Sprintf("%x", b)[:16]
+}
+
+func getDigestAuthrization(digestParts map[string]string) string {
+	d := digestParts
+	ha1 := getMD5(d["username"] + ":" + d["realm"] + ":" + d["password"])
+	ha2 := getMD5(d["method"] + ":" + d["uri"])
+	nonceCount := 00000001
+	cnonce := getCnonce()
+	response := getMD5(fmt.Sprintf("%s:%s:%v:%s:%s:%s", ha1, d["nonce"], nonceCount, cnonce, d["qop"], ha2))
+	return fmt.Sprintf(
+		`Digest username="%s", realm="%s", nonce="%s", uri="%s", cnonce="%s", nc="%v", qop="%s", response="%s"`,
+		d["username"],
+		d["realm"],
+		d["nonce"],
+		d["uri"],
+		cnonce,
+		nonceCount,
+		d["qop"],
+		response,
+	)
+}
+
+type ErrDocNotFound struct{}
+
+func (e *ErrDocNotFound) Error() string {
+	return fmt.Sprintln("there is no element. Maybe already page is created")
+}
+
+func getOriginal(b []byte) (string, error) {
+	doc, err := goquery.NewDocumentFromReader(bytes.NewReader(b))
+	if err != nil {
+		return "", err
+	}
+
+	return doc.Find("textarea[name='original']").Text(), nil
+}
+
+func getDigest(b []byte) (string, error) {
+	doc, err := goquery.NewDocumentFromReader(bytes.NewReader(b))
+	if err != nil {
+		return "", err
+	}
+
+	dgst, ok := doc.Find("input[name='digest']").Attr("value")
+	if !ok {
+		return "", &ErrDocNotFound{}
+	}
+
+	return dgst, nil
+}
--- pkg/memswiki/memswiki.go
+++ pkg/memswiki/memswiki.go
@@ -5,12 +5,35 @@ import (
 	"LabMeeting/pkg/schedule"
 	"fmt"
 	"log"
+	"net/http"
+	"net/url"
 	"os"
 	"regexp"
 	"strconv"
+	"strings"
 
 	"github.com/joho/godotenv"
 )
+
+var (
+	username string
+	password string
+	proxyURL *url.URL
+	memsWiki *url.URL
+)
+
+func init() {
+	if err := godotenv.Load("../config/.env"); err != nil {
+		log.Println(fmt.Errorf("Failed to read \".env\""))
+		return
+	}
+	username = os.Getenv("WIKI_USERNAME")
+	password = os.Getenv("WIKI_PASSWORD")
+	proxyURL, _ = url.Parse(os.Getenv("PROXY_URL"))
+	memsWiki, _ = url.Parse(os.Getenv("MEMSWIKI_URL"))
+
+	return
+}
 
 // only executive
 func WriteTemplate(receptionIssues, nanotechHelpIssues []redmine.Issue, calendarSchdules []*schedule.CalendarSchedule) (string, error) {
@@ -77,4 +100,75 @@ func WriteTemplate(receptionIssues, nanotechHelpIssues []redmine.Issue, calendar
 	log.Printf("The generated template is as follows...\n--------------------------------------------------\n%s\n--------------------------------------------------\n", templateText)
 
 	return templateText, nil
+}
+
+var stdHeader = map[string]string{
+	"Accept":                    "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8",
+	"Accept-Encoding":           "gzip, deflate",
+	"Accept-Language":           "ja,en-US;q=0.7,en;q=0.3",
+	"Cache-Contorl":             "no-cache",
+	"Connection":                "keep-alive",
+	"Content-Length":            "55",
+	"Host":                      "mozart.if.t.u-tokyo.ac.jp",
+	"Origin":                    "http://mozart.if.t.u-tokyo.ac.jp",
+	"Pragma":                    "no-cache",
+	"Upgrade-Insecure-Requests": "1",
+	"User-Agent":                "Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:86.0) Gecko/20100101 Firefox/86.0",
+}
+
+func WriteMinute(date int, msg string) error {
+	log.SetFlags(log.Lshortfile)
+
+	page := "Executive Meeting/" + strconv.Itoa(date) // 作成する議事録ページのアドレス
+
+	// 指定されたページを新規作成するページの準備
+	payloadNewPage := map[string]string{
+		"encode_hint": "ぷ",
+		"plugin":      "newpage",
+		"refer":       "FrontPage",
+		"page":        page,
+	}
+	valuesNewPage := url.Values{}
+	for k, v := range payloadNewPage {
+		valuesNewPage.Add(k, v)
+	}
+	stdHeader["Content-Type"] = "application/x-www-form-urlencoded"
+	body, err := digestPost(http.MethodPost, memsWiki.Scheme+memsWiki.Host, "/memswiki/index.php", stdHeader, strings.NewReader(valuesNewPage.Encode()))
+	if err != nil {
+		return fmt.Errorf("Failed to WriteMinutes(): %w", err)
+	}
+	original, err := getOriginal(body)
+	if err != nil {
+		return fmt.Errorf("Failed to WriteMinutes(): %w", err)
+	}
+	digest, err := getDigest(body)
+	if err != nil {
+		return fmt.Errorf("Failed to WriteMinutes(): %w", err)
+	}
+
+	log.Println(original)
+	log.Println(digest)
+	log.Println(msg)
+
+	// 指定されたページに議事録を登録
+	payloadPost := map[string]string{
+		"encode_hint": "ぷ",
+		"cmd":         "edit",
+		"page":        page,
+		"digest":      digest,
+		"msg":         msg,
+		"write":       "Update",
+		"original":    original,
+	}
+	valuesPost := url.Values{}
+	for k, v := range payloadPost {
+		valuesPost.Add(k, v)
+	}
+	stdHeader["Content-Type"] = "application/x-www-form-urlencoded"
+	body, err = digestPost(http.MethodPost, memsWiki.Scheme+memsWiki.Host, "/memswiki/index.php", stdHeader, strings.NewReader(valuesPost.Encode()))
+	if err != nil {
+		return fmt.Errorf("Failed to WriteMinutes(): %w", err)
+	}
+
+	return nil
 }
