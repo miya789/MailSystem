9f144b46c2fcddc723217eb11a20c2c441689860
miya789 <miya.nori42395@gmail.com>
2021-03-25 16:00:08 +0900

    Modify not to prepare ".exe binary" & [WIP] Add debug

--- mkrel.sh
+++ mkrel.sh
@@ -4,14 +4,13 @@ progname=$1
 
 for os in ${OSS}
 do
-    if [ $os = "windows" ]; then
-        fullname="${progname}_${CI_COMMIT_TAG}_${os}_${GOARCH}.exe"
-    else
-        fullname="${progname}_${CI_COMMIT_TAG}_${os}_${GOARCH}"
+    fullname="${progname}_${CI_COMMIT_TAG}_${os}_${GOARCH}"
 
     fi
 	linkname="${fullname}(SHA256 $(cut -f1 -d' ' ${fullname}.sha256))"
 	linkurl="${BASEURL}/jobs/${CI_JOB_ID}/artifacts/${fullname}"
+
+    echo "${BASEURL}/releases/${CI_COMMIT_TAG}/assets/links"
 
     curl --insecure -v -X POST -H "PRIVATE-TOKEN: ${PRIVATE_TOKEN}" "${BASEURL}/releases/${CI_COMMIT_TAG}/assets/links" \
         -d name=${fullname} \
