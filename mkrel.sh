f77b31c01ed40ddc47bec7347a9782f12e38919d
miya789 <miya.nori42395@gmail.com>
2021-03-25 16:26:07 +0900

    Fix README.md & Remove debug in mkrel.sh

--- mkrel.sh
+++ mkrel.sh
@@ -8,8 +8,6 @@ do
 	linkname="${fullname}(SHA256 $(cut -f1 -d' ' ${fullname}.sha256))"
 	linkurl="${BASEURL}/jobs/${CI_JOB_ID}/artifacts/${fullname}"
 
-    echo "${BASEURL}/releases/${CI_COMMIT_TAG}/assets/links"
-
     curl --insecure -v -X POST -H "PRIVATE-TOKEN: ${PRIVATE_TOKEN}" "${BASEURL}/releases/${CI_COMMIT_TAG}/assets/links" \
         -d name=${fullname} \
         -d url=${linkurl}
